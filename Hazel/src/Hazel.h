204a0a859fa555795466215437bcc636ed508964
Gavish Bansal <gavybansal9911@gmail.com>
2023-12-13 10:27:47 +0530

    Entry Point

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+// For use by Hazel applications.
+#include "Hazel/Application.h"
+
+// ----------Entry Point----------
+#include "Hazel/EntryPoint.h"
+// ----------Entry Point----------
